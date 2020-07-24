68d63f113764d2bc1be58b3bc69bebadc68f888b
aaron-schuyler <asky28@gmail.com>
2020-07-23 21:04:30 -0400

    fleshed out the form for meals and added method to meal to list ingredients and quantities from join table

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,4 +4,14 @@ class Meal < ApplicationRecord
   has_many :ingredients, through: :ingredients_meals
   has_many :meals_days
   accepts_nested_attributes_for :ingredients_meals
+
+  def ingredients_list
+    im = {}
+    self.ingredients_meals.each do |i|
+      im[i.ingredient_id] = i.servings
+    end
+    self.ingredients.each do |i|
+      i.servings = im[i.id]
+    end
+  end
 end
