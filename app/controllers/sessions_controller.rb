d366690aad202a1dea765e774712d1407fa0b431
aaron-schuyler <asky28@gmail.com>
2020-07-29 14:46:56 -0400

    login and signup forms stylized

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    @on = true
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete '/logout', to: "sessions#destroy"
+  delete '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'days#index'
   get '/today', to: 'days#today'
